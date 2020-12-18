5e15878259d2babbbb6f02c7c81ebb70a1e47b31
Znth3 <zynthkas@gmail.com>
2020-12-18 15:09:45 +0700

    fix class ini Penduduk.php

--- app/Models/Penduduk.php
+++ app/Models/Penduduk.php
@@ -10,20 +10,20 @@ class Penduduk extends Model
     use HasFactory;
     protected $table = 'penduduk';
 
-    public function kartuKeluarga(){
-        return $this->belongsTo('kartu_keluarga', 'keluarga_id', 'id');
+    public function keluarga(){
+        return $this->belongsTo(KartuKeluarga::class, 'keluarga_id', 'id');
     }
 
     public function levelPendidikan(){
-        return $this->belongsTo('level_pendidikan');
+        return $this->belongsTo(LevelPendidikan::class);
     }
 
     public function kewarganegaraan(){
-        return $this->belongsTo('kewarganegaraan');
+        return $this->belongsTo(Kewarganegaraan::class);
     }
 
     public function pekerjaan(){
-        return $this->belongsTo('pekerjaan');
+        return $this->belongsTo(Pekerjaan::class);
     }
 
     public $timestamps = false;
